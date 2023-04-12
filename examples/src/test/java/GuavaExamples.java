9576fb00c9d7d6cabaf7742f9dd82f853e965796
E19J736E <reda.essekkaki@etu.univ-nantes.fr>
2023-04-12 11:17:14 +0200

    First tests with Guava

--- examples/src/test/java/GuavaExamples.java
+++ examples/src/test/java/GuavaExamples.java
@@ -1,16 +1,24 @@
 
 import org.junit.Test;
+import com.google.common.base.Preconditions;
 
 public class GuavaExamples {
 
-    @Test
-    public void test(){
-
-
-
-
-
-
+    public void doSomething(String arg){
+        Preconditions.checkNotNull(arg, "An argument should be given");
     }
+    @Test
+    public void testNullArgument(){
+        String test = null;
+        doSomething(test);
+    }
+
+
+    @Test
+    public void testNotNullArgument(){
+        String test = "Hello world";
+        doSomething(test);
+    }
+
 
 }
