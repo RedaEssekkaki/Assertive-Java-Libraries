1959a1837c2bdfc88cfa4e1b033e20d58af7d97a
Theo Arga <theo.arga@etu.univ-nantes.fr>
2023-04-12 10:59:16 +0200

    truth tests

--- examples/src/test/java/TruthExamples.java
+++ examples/src/test/java/TruthExamples.java
@@ -8,17 +8,31 @@ import static com.google.common.truth.Truth.assertWithMessage;
 public class TruthExamples {
 
     @Test
-    public void test()
+    public void intTest()
     {
         Integer i = 20;
         Integer j = 30;
 
+        assertThat(i).isInstanceOf(Integer.class);
+
         assertThat(i).isEqualTo(20);
-        assertThat(i).isEqualTo(10);
 
         assertThat(j).isGreaterThan(i);
+
+        assertThat(i).isEqualTo(10);
     }
 
+    @Test
+    public void floatTests(){
+        float f1 = 1.0f;
+        float f2 = 1.1f;
+
+
+        assertThat(f1).isWithin(0.11f).of(f2);
+
+        assertThat(f1).isEqualTo(f2);
+
+    }
 
 
 }
