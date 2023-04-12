7b12207eac54fa31036de50a43caf79a03971f53
E19J736E <reda.essekkaki@etu.univ-nantes.fr>
2023-04-12 15:08:55 +0200

    Guava tests

--- examples/src/test/java/GuavaExamples.java
+++ examples/src/test/java/GuavaExamples.java
@@ -1,11 +1,31 @@
 
+import junit.framework.AssertionFailedError;
 import org.junit.Test;
 import com.google.common.base.Preconditions;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
 public class GuavaExamples {
 
+    private static final Object IGNORE_ME =
+            new Object() {
+                @Override
+                public String toString() {
+                    throw new AssertionFailedError();
+                }
+            };
+
+    private static class Message {
+        boolean invoked;
+
+        @Override
+        public String toString() {
+            assertFalse(invoked);
+            invoked = true;
+            return "A message";
+        }
+    }
 
     /**
      * Testing the checkArgument Guard in Guava
@@ -30,5 +50,9 @@ public class GuavaExamples {
         Preconditions.checkNotNull(test, "An argument tot the function should be given");
     }
 
+    @Test
+    public void testCheckState() {
+        Preconditions.checkState(true, "%s", IGNORE_ME);
+    }
 
 }
