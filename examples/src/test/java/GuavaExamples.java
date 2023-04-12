79b928b5b502455460f77589a3f4cd9d7372331f
E19J736E <reda.essekkaki@etu.univ-nantes.fr>
2023-04-12 15:27:17 +0200

    Guava tests

--- examples/src/test/java/GuavaExamples.java
+++ examples/src/test/java/GuavaExamples.java
@@ -8,6 +8,45 @@ import static org.junit.Assert.fail;
 
 public class GuavaExamples {
 
+
+    /**
+     * Testing the checkArgument Guard in Guava
+     * */
+    @Test
+    public void testCheckArgument() {
+        try {
+            Preconditions.checkArgument(false);
+            fail("no exception thrown, IllegalArgumentException expected");
+        } catch (IllegalArgumentException expected) {
+        }
+    }
+    @Test
+    public void testTrueExpression(){
+        Preconditions.checkArgument(true);
+    }
+
+    /**
+     * Testing the checkNotNull Guard in Guava
+     * */
+    @Test
+    public void testNotNullArgument(){
+        try{
+            Preconditions.checkNotNull(null, "An argument to the function should be given");
+            fail("A NullPointerException was expected");
+        }
+        catch (NullPointerException expected){
+        }
+    }
+    @Test
+    public void testNotNullArgument_successful(){
+        Preconditions.checkNotNull("A message", "An argument to the function should be given");
+    }
+
+    @Test
+    public void testCheckState() {
+        Preconditions.checkState(true, "%s", IGNORE_ME);
+    }
+
     private static final Object IGNORE_ME =
             new Object() {
                 @Override
@@ -26,33 +65,4 @@ public class GuavaExamples {
             return "A message";
         }
     }
-
-    /**
-     * Testing the checkArgument Guard in Guava
-     *
-     * */
-    @Test
-    public void testCheckArgument() {
-        try {
-            Preconditions.checkArgument(false);
-            fail("no exception thrown, IllegalArgumentException expected");
-        } catch (IllegalArgumentException expected) {
-        }
-    }
-    @Test
-    public void testTrueExpression(){
-        Preconditions.checkArgument(true);
-    }
-
-    @Test
-    public void testNotNullArgument(){
-        String test = "Hello world";
-        Preconditions.checkNotNull(test, "An argument tot the function should be given");
-    }
-
-    @Test
-    public void testCheckState() {
-        Preconditions.checkState(true, "%s", IGNORE_ME);
-    }
-
 }
