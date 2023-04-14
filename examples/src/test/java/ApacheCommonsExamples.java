41af0f7a60e140d588d9edb69982bd256345a74b
Emmnuel STALDER <emmanuel.stalder@etu.univ-nantes.fr>
2023-04-14 10:03:10 +0200

    Debug ApacheCommonsExamples

--- examples/src/test/java/ApacheCommonsExamples.java
+++ examples/src/test/java/ApacheCommonsExamples.java
@@ -1,10 +1,11 @@
 
 import org.apache.commons.lang3.Validate;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * The purpose of this class is to tinker with apache.commons.lang3.Validate 's
@@ -17,16 +18,7 @@ public class ApacheCommonsExamples {
 
     @BeforeEach
     public void setup () {
-        ArrayList<Double> grades = new ArrayList<Double>() {{
-            add(12.5);
-            add(5.25);
-            add(8.75);
-            add(15.5);
-            add(18.0);
-            add(11.25);
-            add(13.75);
-            add(14.0);
-        }};
+        ArrayList<Double> grades = new ArrayList<>(Arrays.asList(12.5, 5.25, 8.75, 15.5, 18.0, 11.25, 13.75, 14.0));
         testedStudent.set(grades);
     }
 
@@ -46,7 +38,9 @@ public class ApacheCommonsExamples {
         try {
             testedStudent.set(grades);
             fail("Ths test is supposed to fail.");
-        } catch (IllegalArgumentException expected) {}
+        } catch (IllegalArgumentException expected) {
+            System.out.println(expected);
+        }
     }
 
     @Test
@@ -63,7 +57,9 @@ public class ApacheCommonsExamples {
         try {
             testedStudent.set(grades);
             fail("This test is supposed to fail.");
-        } catch (IllegalArgumentException expected) {}
+        } catch (IllegalArgumentException expected) {
+            System.out.println(expected);
+        }
     }
 
     @Test
@@ -71,7 +67,9 @@ public class ApacheCommonsExamples {
         try {
             testedStudent.setGrade(0, null);
             fail("This test is supposed to fail.");
-        } catch (IndexOutOfBoundsException expected) {}
+        } catch (IndexOutOfBoundsException expected) {
+            System.out.println(expected);
+        }
     }
 
     @Test
@@ -79,27 +77,41 @@ public class ApacheCommonsExamples {
         try {
             testedStudent.setGrade(12, 18.0);
             fail("This test is supposed to fail.");
-        } catch (IndexOutOfBoundsException expected) {}
+        } catch (IndexOutOfBoundsException expected) {
+            System.out.println(expected);
+        }
     }
 
     @Test
     public void testAVG () {
         try {
-            testedStudent.setGrade(5, -2.0);
+            testedStudent.setGrade(3, -2.0);
             Double temp = testedStudent.avg();
+            System.out.println(temp);
             fail("This test is supposed to fail.");
-        } catch (IndexOutOfBoundsException expected) {}
+        } catch (IllegalArgumentException expected) {
+            System.out.println(expected);
+        }
+    }
+
+    @Test
+    public void testNormalSet () {
+        testedStudent.setGrade(2, 18.5);
     }
 
     /**
      * Private class to set up methods with guard clauses to use in the tests
      */
     private class StudentGrade {
-        private ArrayList<Double> grades = new ArrayList<>(8);
+        private ArrayList<Double> grades;
+        
+        public StudentGrade () {
+            this.grades = new ArrayList<Double>(8);
+        }
 
         public void set(ArrayList<Double> newGrades) {
             Validate.isTrue(newGrades.size() == 8, "You can't have more or less grades than you have courses: current %d ; required 8", newGrades.size());
-            this.grades = newGrades;
+            this.grades = new ArrayList<>(newGrades);
         }
 
         public ArrayList<Double> get() {
@@ -110,19 +122,19 @@ public class ApacheCommonsExamples {
         public void setGrade(int courseNumber, Double newGrade) {
             Validate.validIndex(this.grades, courseNumber, "There are only 8 courses in this school.");
             Validate.notNull(newGrade, "You can't have a null grade.");
-            this.grades.set(courseNumber - 1, newGrade);
+            this.grades.set(courseNumber, newGrade);
         }
 
         @org.jetbrains.annotations.NotNull
         public Double avg () {
             Validate.notEmpty(grades, "You must have a grade for each course");
-            Double divisor = Double.valueOf(0.0);
-            Double divider = Double.valueOf(0.0);
+            double divisor = 0.0;
+            double divider = 0.0;
 
             for (Double each : grades) {
                 Validate.isTrue(each >= 0, "A grade cannot be lesser than 0: %d", each);
                 divisor += each;
-                divider += Double.valueOf(1.0);
+                divider += 1.0;
             }
             return divisor / divider;
         }
