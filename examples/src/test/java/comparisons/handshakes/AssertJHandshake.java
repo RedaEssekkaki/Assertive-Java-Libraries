19a0a17a3ddcb880160d633f01d18870e003dcd7
E19J736E <reda.essekkaki@etu.univ-nantes.fr>
2023-04-14 13:15:02 +0200

    adding assertionJ handshake tests

--- examples/src/test/java/comparisons/handshakes/AssertJHandshake.java
+++ examples/src/test/java/comparisons/handshakes/AssertJHandshake.java
@@ -29,6 +29,7 @@ public class AssertJHandshake {
     public void testSet()
     {
         //test all get() is null
+
         assertThat(BDA_0.get()).isNull();
         assertThat(BDA_1.get()).isNull();
         assertThat(BDA_2.get()).isNull();
@@ -38,16 +39,25 @@ public class AssertJHandshake {
         BDA_0.set(BDA_1);
 
         //test bda_0 & bda_1 associated
+        assertThat(BDA_0.get()).isEqualTo(BDA_1);
+        assertThat(BDA_1.get()).isEqualTo(BDA_0);
+
 
         BDA_1.set(BDA_2);
 
-        //test bda_0 is null
-
         //test bda_2 & bda_1 linked
+        assertThat(BDA_1.get()).isEqualTo(BDA_2);
+        assertThat(BDA_2.get()).isEqualTo(BDA_1);
 
-        BDA_3.set(BDA_0);
+
+        //test bda_0 is null
+        assertThat(BDA_0.get()).isNull();
+
 
         //test bda_3 & bda_0 linked
+        BDA_3.set(BDA_0);
+        assertThat(BDA_0.get()).isEqualTo(BDA_3);
+        assertThat(BDA_3.get()).isEqualTo(BDA_0);
 
     }
 
@@ -58,15 +68,23 @@ public class AssertJHandshake {
         BDA_2.set(BDA_3);
 
         //test all is not get() null
+        assertThat(BDA_0.get()).isNotNull();
+        assertThat(BDA_1.get()).isNotNull();
+        assertThat(BDA_2.get()).isNotNull();
+        assertThat(BDA_3.get()).isNotNull();
+
 
         BDA_3.unSet();
 
-        //test BDA-3 & BDA_2 get() null
+        //test BDA_3 & BDA_2 get() null
+        assertThat(BDA_3.get()).isNull();
+        assertThat(BDA_2.get()).isNull();
 
         BDA_0.unSet();
 
         //test bda_0 & bda_1 get() null
-
+        assertThat(BDA_1.get()).isNull();
+        assertThat(BDA_0.get()).isNull();
 
 
     }
